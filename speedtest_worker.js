7ec2be02b4b78c90c3f45e7cb06d5797fc5f95e3
Iulia Ionita <iulia@wolfpack-digital.com>
2022-06-02 17:12:10 +0300

    Updates

--- speedtest_worker.js
+++ speedtest_worker.js
@@ -47,18 +47,18 @@ var settings = {
     time_ulGraceTime: 3, //time to wait in seconds before actually measuring ul speed (wait for buffers to fill)
     time_dlGraceTime: 1.5, //time to wait in seconds before actually measuring dl speed (wait for TCP window to increase)
     count_ping: 10, // number of pings to perform in ping test
-    url_dl: "https://disi.magenta.at:8089/fw-speedtest/backend/garbage.xyz", // path to a large file or garbage.php, used for download test. must be relative to this js file
-    url_ul: "https://disi.magenta.at:8089/fw-speedtest/backend/empty.xyz", // path to an empty file, used for upload test. must be relative to this js file
+    url_dl: "https://speedtest.de.telekom-vpn.net/download/garbage.php", // path to a large file or garbage.php, used for download test. must be relative to this js file
+    url_ul: "https://speedtest.de.telekom-vpn.net/upload/empty.php", // path to an empty file, used for upload test. must be relative to this js file
     url_ping: "https://speedtest.de.telekom-vpn.net/", // path to an empty file, used for ping test. must be relative to this js file
     url_getIp: "backend/getIP.php", // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
     getIp_ispInfo: true, //if set to true, the server will include ISP info with the IP address
     getIp_ispInfo_distance: "km", //km or mi=estimate distance from server in km/mi; set to false to disable distance estimation. getIp_ispInfo must be enabled in order for this to work
     xhr_dlMultistream: 6, // number of download streams to use (can be different if enable_quirks is active)
-    xhr_ulMultistream: 1, // number of upload streams to use (can be different if enable_quirks is active)
+    xhr_ulMultistream: 3, // number of upload streams to use (can be different if enable_quirks is active)
     xhr_multistreamDelay: 300, //how much concurrent requests should be delayed
     xhr_ignoreErrors: 1, // 0=fail on errors, 1=attempt to restart a stream if it fails, 2=ignore all errors
     xhr_dlUseBlob: false, // if set to true, it reduces ram usage but uses the hard drive (useful with large garbagePhp_chunkSize and/or high xhr_dlMultistream)
-    xhr_ul_blob_megabytes: 0.25, //size in megabytes of the upload blobs sent in the upload test (forced to 4 on chrome mobile)
+    xhr_ul_blob_megabytes: 20, //size in megabytes of the upload blobs sent in the upload test (forced to 4 on chrome mobile)
     garbagePhp_chunkSize: 100, // size of chunks sent by garbage.php (can be different if enable_quirks is active)
     enable_quirks: true, // enable quirks for specific browsers. currently it overrides settings to optimize for specific browsers, unless they are already being overridden with the start command
     ping_allowPerformanceApi: true, // if enabled, the ping test will attempt to calculate the ping more precisely using the Performance API. Currently works perfectly in Chrome, badly in Edge, and not at all in Firefox. If Performance API is not supported or the result is obviously wrong, a fallback is provided.
@@ -442,7 +442,7 @@ function ulTest(done) {
     var reqsmall = [];
     for (var i = 0; i < settings.xhr_ul_blob_megabytes; i++) req.push(r);
     req = new Blob(req);
-    r = new ArrayBuffer(128);
+    r = new ArrayBuffer(262144);
     try {
         r = new Uint32Array(r);
         for (var i = 0; i < r.length; i++) r[i] = Math.random() * maxInt;
