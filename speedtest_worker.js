2a92186eb529197f27c4dd4b88a356c1ab038be8
Iulia Ionita <iulia@wolfpack-digital.com>
2022-06-03 10:36:27 +0300

    Add cors

--- speedtest_worker.js
+++ speedtest_worker.js
@@ -48,7 +48,7 @@ var settings = {
     time_dlGraceTime: 1.5, //time to wait in seconds before actually measuring dl speed (wait for TCP window to increase)
     count_ping: 10, // number of pings to perform in ping test
     url_dl: "https://speedsec.performance-metrics.de/download/garbage.php", // path to a large file or garbage.php, used for download test. must be relative to this js file
-    url_ul: "https://speedsec.performance-metrics.de/upload/empty.php", // path to an empty file, used for upload test. must be relative to this js file
+    url_ul: "https://speedsec.performance-metrics.de/upload/empty.php?cors=true", // path to an empty file, used for upload test. must be relative to this js file
     url_ping: "https://speedtest.de.telekom-vpn.net/", // path to an empty file, used for ping test. must be relative to this js file
     url_getIp: "backend/getIP.php", // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
     getIp_ispInfo: true, //if set to true, the server will include ISP info with the IP address
