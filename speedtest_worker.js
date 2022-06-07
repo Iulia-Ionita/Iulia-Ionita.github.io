d67322b47384e5a251cf2ecb3719924c2bb18e37
Alexandru-Iosif Mazarache <alexandru.mazarache@endava.com>
2022-06-07 11:02:05 +0300

    Set dlMultistream to 3

--- speedtest_worker.js
+++ speedtest_worker.js
@@ -48,12 +48,12 @@ var settings = {
     time_dlGraceTime: 1.5, //time to wait in seconds before actually measuring dl speed (wait for TCP window to increase)
     count_ping: 10, // number of pings to perform in ping test
     url_dl: "https://speedsec.performance-metrics.de/download/garbage.php", // path to a large file or garbage.php, used for download test. must be relative to this js file
-    url_ul: "https://speedsec.performance-metrics.de/upload/empty.php?cors=true", // path to an empty file, used for upload test. must be relative to this js file
+    url_ul: "https://speedsec.performance-metrics.de/upload/empty.php", // path to an empty file, used for upload test. must be relative to this js file
     url_ping: "https://speedtest.de.telekom-vpn.net/", // path to an empty file, used for ping test. must be relative to this js file
     url_getIp: "backend/getIP.php", // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
     getIp_ispInfo: true, //if set to true, the server will include ISP info with the IP address
     getIp_ispInfo_distance: "km", //km or mi=estimate distance from server in km/mi; set to false to disable distance estimation. getIp_ispInfo must be enabled in order for this to work
-    xhr_dlMultistream: 6, // number of download streams to use (can be different if enable_quirks is active)
+    xhr_dlMultistream: 3, // number of download streams to use (can be different if enable_quirks is active)
     xhr_ulMultistream: 3, // number of upload streams to use (can be different if enable_quirks is active)
     xhr_multistreamDelay: 300, //how much concurrent requests should be delayed
     xhr_ignoreErrors: 1, // 0=fail on errors, 1=attempt to restart a stream if it fails, 2=ignore all errors
